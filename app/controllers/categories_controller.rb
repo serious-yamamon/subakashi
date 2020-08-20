93d988170c6bc446a7202e32294d838fd85e54ab
serious-yamamon <mkm2578y@gmail.com>
2020-08-21 01:35:05 +0900

    202008216

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,5 @@ class CategoriesController < ApplicationController
     @category = Category.find_by(name: params[:id])
     @posts = @category.posts.published.default_order.page(params[:page]).per(12) if @category.posts
     @slider_posts = Post.published.favorite.default_order.limit(8)
+  end
 end
